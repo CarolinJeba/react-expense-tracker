e22f2dd19e8e78abf1d96b7c883f32f090095d40
--replace-all <r.carolinjeba@outlook.com>
2022-03-01 13:29:04 +0530

    State anaged using React Context for transactions

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
-import { AddTransactionComponent } from './components/AddTransactionComponent';
+import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className='container'>
         <Balance />
         <IncomeExpenses />
         <TransactionList />
-        <AddTransactionComponent />
+        <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
